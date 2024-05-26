a5599853bfcc4d1f1b1b75d1118ccbc5c6c38d35
Mussie Alemayehu <157012749+mussie-alemayehu@users.noreply.github.com>
2024-05-26 10:26:46 +0300

    switch to php files

--- dashboard/index.php
+++ dashboard/index.php
@@ -20,51 +20,10 @@
 </head>
 
 <body>
-    <div class="side-nav">
-        <img src="../education-hat.png" alt="Education hat">
-        <div class="nav-item">
-            <a href="">
-                <i class="nav-icon fa-solid fa-table-columns"></i>
-                Dashboard
-            </a>
-        </div>
-
-        <div class="nav-item">
-            <a href="">
-                <i class="nav-icon fa-solid fa-pen"></i>
-                Registration
-            </a>
-        </div>
-
-        <div class="nav-item">
-            <a href="">
-                <i class="nav-icon fa-regular fa-rectangle-list"></i>
-                Courses
-            </a>
-        </div>
-
-        <div class="nav-item">
-            <a href="">
-                <i class="nav-icon fa-solid fa-square-poll-vertical"></i>
-                Results
-            </a>
-        </div>
-
-        <div class="nav-item">
-            <a href="">
-                <i class="nav-icon fa-solid fa-message"></i>
-                Chat
-            </a>
-        </div>
-
-        <div class="nav-item">
-            <a href="">
-                <i class="nav-icon fa-solid fa-right-from-bracket"></i>
-                Logout
-            </a>
-        </div>
-
-    </div>
+    <?php
+        // include the side navigation bar 
+        include("../side-nav.php"); 
+    ?>
     <div class="main-contents">
         <div class="profile">
             <p>username</p>
---
+++ side-nav.php
@@ -0,0 +1,44 @@
+<div class="side-nav">
+    <img src="../education-hat.png" alt="Education hat">
+    <div class="nav-item">
+        <a href="">
+            <i class="nav-icon fa-solid fa-table-columns"></i>
+            Dashboard
+        </a>
+    </div>
+
+    <div class="nav-item">
+        <a href="">
+            <i class="nav-icon fa-solid fa-pen"></i>
+            Registration
+        </a>
+    </div>
+
+    <div class="nav-item">
+        <a href="">
+            <i class="nav-icon fa-regular fa-rectangle-list"></i>
+            Courses
+        </a>
+    </div>
+
+    <div class="nav-item">
+        <a href="">
+            <i class="nav-icon fa-solid fa-square-poll-vertical"></i>
+            Results
+        </a>
+    </div>
+
+    <div class="nav-item">
+        <a href="">
+            <i class="nav-icon fa-solid fa-message"></i>
+            Chat
+        </a>
+    </div>
+
+    <div class="nav-item">
+        <a href="">
+            <i class="nav-icon fa-solid fa-right-from-bracket"></i>
+            Logout
+        </a>
+    </div>
+</div>
