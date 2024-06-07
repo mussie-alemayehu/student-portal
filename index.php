f6efc00056cf80f8a343e74306c30f2831eef65b
Mussie Alemayehu <157012749+mussie-alemayehu@users.noreply.github.com>
2024-06-07 13:10:52 +0300

    allow loading dashboard with side navigation click

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="side-nav-styles.css">
     <link rel="stylesheet" href="dashboard/main-contents-styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://kit.fontawesome.com/13dd5a24f4.js" crossorigin="anonymous"></script>
     <style>
         /* internal style required to overwrite the web styles of the icons */
@@ -17,6 +18,14 @@
             color: rgb(227, 223, 223);
         }
     </style>
+    <script>
+        $(document).ready(function() {
+            $('#dashboard-link').click(function(event) {
+                event.preventDefault();
+                $('#main-contents').load('dashboard/index.php');
+            });
+        });
+    </script>
 </head>
 
 <body>
@@ -26,17 +35,13 @@
 
     // send the user to the login screen if there is no session info
     if (!isset($_SESSION["user_id"]) || !isset($_SESSION["username"])) {
-        // header("Location: auth/index.php");
-        echo("no registered student found");
+        header("Location: auth/index.php");
+        exit();
     }
     include("side-nav.php");
     ?>
 
-    <div class="main-contents">
-        <?php
-        include("dashboard/index.php");
-        ?>
-    </div>
+    <div id="main-contents"></div>
 
 </body>
 
--- side-nav.php
+++ side-nav.php
@@ -1,42 +1,42 @@
 <div class="side-nav">
     <img src="education-hat.png" alt="Education hat">
     <div class="nav-item">
-        <a href="">
+        <a href="#" id="dashboard-link">
             <i class="nav-icon fa-solid fa-table-columns"></i>
             Dashboard
         </a>
     </div>
 
     <div class="nav-item">
-        <a href="">
+        <a href="#" id="registration-link">
             <i class="nav-icon fa-solid fa-pen"></i>
             Registration
         </a>
     </div>
 
     <div class="nav-item">
-        <a href="">
+        <a href="#" id="courses-link">
             <i class="nav-icon fa-regular fa-rectangle-list"></i>
             Courses
         </a>
     </div>
 
     <div class="nav-item">
-        <a href="">
+        <a href="#" id="results-link">
             <i class="nav-icon fa-solid fa-square-poll-vertical"></i>
             Results
         </a>
     </div>
 
     <div class="nav-item">
-        <a href="">
+        <a href="#" id="chat-link">
             <i class="nav-icon fa-solid fa-message"></i>
             Chat
         </a>
     </div>
 
     <div class="nav-item">
-        <a href="">
+        <a href="#" id="logout-link">
             <i class="nav-icon fa-solid fa-right-from-bracket"></i>
             Logout
         </a>
